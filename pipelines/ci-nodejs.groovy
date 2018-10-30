3ccdca66d01a53588cc60d54e533fa09aa0b9458
ivozzo <alessandro.ivaldi@email.it>
2018-10-30 10:57:45 +0100

    Deleted old pipeline as it was temporary

--- pipelines/ci-nodejs.groovy
+++ pipelines/ci-nodejs.groovy
@@ -6,24 +6,7 @@ def call(Map pipelineParams) {
   def sonarScm = "";
   def dockerArgs = "";
 
-  if(pipelineParams.dockerArgs!=null)
-  	dockerArgs = pipelineParams.dockerArgs;
-  
-  if(pipelineParams.dockerImageType!=null)
-  	dockerImageType = pipelineParams.dockerImageType;
-  else
-    dockerImageType = "jenkinsci/jnlp-slave";
-
   pipeline{
-    agent{ label "dev" }
-
-    options {
-      buildDiscarder(logRotator(artifactNumToKeepStr: '1',numToKeepStr: '20'))
-      timestamps()
-    }
-    triggers{
-      pollSCM(pipelineParams.trigger)
-    }
     stages{
       stage('Checkout'){
         steps{
@@ -33,16 +16,16 @@ def call(Map pipelineParams) {
       stage('Build'){
       	agent{
       	  docker {
-             tool name: 'NodeJS-10', type: 'nodejs'
-      	     reuseNode true
-             image dockerImageType+":"+pipelineParams.dockerImage
-		     args "--name='$BUILD_TAG-Build' -e TZ=Europe/Rome $dockerArgs"
+            image 'node:10-alpine'
+		        args "--name='$BUILD_TAG-Build' -e TZ=Europe/Rome $dockerArgs"
       	  }
-    	}
+    	  }
+        steps {
+          sh 'node --version'
+        }
       }
     }
     post {
-      // always means, well, always run.
       always {
         cleanWs()
       }
